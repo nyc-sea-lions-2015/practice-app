edc5f2ba1771918cebaf9cd626ebab6fa8977b3f
Kurt Schnelker <kurt.schnelker@gmail.com>
2015-04-01 10:29:17 -0400

    creates controller files, adds column to users table

--- db/migrate/20150401092159_create_users.rb
+++ db/migrate/20150401092159_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
+      t.integer :practice_sessions
 
       t.timestamps null: false
     end
