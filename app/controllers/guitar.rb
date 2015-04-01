48406baf4132cd7459e62a4b8fda1faa3dd29563
Kurt Schnelker <kurt.schnelker@gmail.com>
2015-04-01 15:59:28 -0400

    adds to all views

--- app/controllers/guitar.rb
+++ app/controllers/guitar.rb
@@ -3,3 +3,13 @@ get '/guitars' do
 
   erb :guitars
 end
+
+get '/guitar/new' do
+  @guitars = Guitar.where(user_id: session[:user_id])
+  erb :guitars
+end
+
+post '/guitar/new' do
+  Guitar.create(user_id: session[:user_id], style: params['guitar_style'])
+  redirect '/guitars'
+end
